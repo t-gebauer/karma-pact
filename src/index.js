2bfbd27a49466ba16461f217231aaf75744692be
Timo Gebauer <t-gebauer@users.noreply.github.com>
2020-08-10 10:57:46 +0200

    feat: async karma plugin, forward error
    Karma supports asynchronous framework initialization since 4.3.0 (2019-08-27)
    
    With this it is possible to fail the karma startup if the pact server can not be started (e.g. port is unavailable) instead of blocking forever.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-var wrapper = require('@pact-foundation/pact-node');
-var deasync = require('deasync');
+const wrapper = require('@pact-foundation/pact-node');
 
-var runPactMockServer = function (pacts, logger) {
-	var log = logger.create('pact');
+const runPactMockServer = function (pacts, logger) {
+	const log = logger.create('pact');
 	pacts = pacts || [];
 
 	// If pact options is object, wrap in array
@@ -10,28 +9,15 @@ var runPactMockServer = function (pacts, logger) {
 		pacts = [pacts];
 	}
 
-	var startingServers = [];
-
-	pacts.map(function (pact) {
-		var server = wrapper.createServer(pact);
-		server.start().then(function () {
+	return Promise.all(pacts.map((pact) => {
+		const server = wrapper.createServer(pact);
+		return server.start().then(() => {
 			log.info('Pact Mock Server running on port: ' + server.options.port);
-			// Remove current server from starting servers array
-			startingServers = startingServers.filter(x => x !== server.options.port);
-		}, function (err) {
+		}, err => {
 			log.error('Error while trying to run karma-pact: ' + err);
+			throw err;
 		});
-		// Add current server to starting servers array
-		startingServers.push(server.options.port);
-	});
-
-	deasync.loopWhile(function () {
-		return !isMockServerReady();
-	});
-
-	function isMockServerReady() {
-		return startingServers.length === 0;
-	}
+	}));
 };
 
 runPactMockServer.$inject = ['config.pact', 'logger'];
